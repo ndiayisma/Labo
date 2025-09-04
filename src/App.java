b39c6dd9ffaffbd86687324c235b67efb9df1400
ndiayisma <ismail.ndiaye@lycee-faure.fr>
2025-09-04 11:21:40 +0200

    Collection de souris

--- src/App.java
+++ src/App.java
@@ -1,13 +1,27 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
         
-        Souris s1 = new Souris(50, "blanche", 2);
+        ArrayList<Souris> souris = new ArrayList<Souris>();
+        souris.add(new Souris(50, "blanche", 2));
+        souris.add(new Souris(45, "grise"));
+        souris.add(new Souris(souris.get(1)));
+        for(Souris s : souris){
+            System.out.println(s);
+        }
+
+
+
+        /*Souris s1 = new Souris(50, "blanche", 2);
         Souris s2 = new Souris(45, "grise");
         Souris s3 = new Souris(s2);
+        Souris s4 = new Souris(s3);
         
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s3);
+        System.out.println(s4);
         
         s1.vieillir();
         s1.vieillir();
@@ -16,10 +30,12 @@ public class App {
         s1.evolue();
         s2.evolue();
         s3.evolue();
+        s4.evolue();
         
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s3);
+        System.out.println(s4);*/
                 
     }
 }
