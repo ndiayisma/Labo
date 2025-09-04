b8e57f622fa79e05aec47ea8a26b6e883837e97e
ndiayisma <ismail.ndiaye@lycee-faure.fr>
2025-09-04 10:56:54 +0200

    Création d'une nouvelle souris

--- src/Souris.java
+++ src/Souris.java
@@ -8,6 +8,28 @@ public class Souris {
 
     public static final int ESPERANCE_VIE = 36;
 
+    public Souris(int poids, String couleur, int age, int esperanceVie) {
+        if(poids < 0) {
+            throw new IllegalArgumentException("Le poids ne peut pas être négatif");
+        }else{
+            this.poids = poids;
+        }
+        if(age < 0) {
+            throw new IllegalArgumentException("L'âge ne peut pas être négatif");
+        }else{
+            this.age = age;
+        }
+        this.couleur = couleur;
+        if(esperanceVie < 0) {
+            throw new IllegalArgumentException("L'espérance de vie ne peut pas être négative");
+        }else{
+            this.esperanceVie = esperanceVie;
+        }
+        this.clonee = false;
+        
+        System.out.println("Une nouvelle souris !");
+        
+    }
 
     public Souris(int poids, String couleur, int age) {
         this.poids = poids;
@@ -19,18 +41,21 @@ public class Souris {
         System.out.println("Une nouvelle souris !");
     }
 
-    public Souris(int poids, String couleur) {
-        this.poids = poids;
-        this.couleur = couleur;
-        this.age = 0;
-        this.esperanceVie = ESPERANCE_VIE;
-        this.clonee = false;
-
-        System.out.println("Une nouvelle souris !");
-    }
+    
   
 
+    public Souris(Souris s2) {
+        this.poids = s2.getPoids();
+        this.couleur = s2.getCouleur();
+        this.age = s2.getAge();
+        this.esperanceVie = (s2.getEsperanceVie()*4)/5;
+        this.clonee = true;
+
+        System.out.println("Une nouvelle souris clonée !");
+    }
+
     
+
     @Override
     public String toString() {
         String result = "Une souris " + this.couleur;
@@ -41,7 +66,25 @@ public class Souris {
         return result; 
      }
 
-    
+    public int getPoids() {
+        return poids;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public boolean isClonee() {
+        return clonee;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getEsperanceVie() {
+        return esperanceVie;
+    }
 
     
 
